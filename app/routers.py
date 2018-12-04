f1f046050be754cd64176559a40e7ef385de62bc
rucmxp <m755213996@gmail.com>
2018-12-04 17:54:22 +0800

    upload.html 用户上传页

--- app/routers.py
+++ app/routers.py
@@ -12,7 +12,12 @@ from app import app
 from app import db
 from .models import User, Client, Record, Share, Photo, Video
 # 记得在config.py修改数据库配置,同时在终端使用db.create_all()命令创建表
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/testdb?charset=utf8'
 
 @app.route('/index') #主页
 def index():
     return render_template('index.html')
+
+@app.route('/author/<authoruid>', methods=['GET', 'POST']) 
+def authorinfo():
+	return render_template('author-info.html')
