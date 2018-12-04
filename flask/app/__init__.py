deae227efe85764794c9fb0d321bd0478d0397b1
lijunjie <853076819@qq.com>
2018-12-04 08:02:50 +0800

    删除所有

--- flask/app/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Date    : 2018-11-13 09:10:59
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
-
-import os
--- flask/app/model/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Date    : 2018-11-13 10:13:28
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
-
-import os
